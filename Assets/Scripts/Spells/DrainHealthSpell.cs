9a0967e175aeb0d200fc888b0caef5eef8f6ff7e
MarkusSecundus <kubahronik.g@gmail.com>
2026-02-09 22:02:10 +0100

    add Photon

--- Assets/Scripts/Spells/DrainHealthSpell.cs
+++ Assets/Scripts/Spells/DrainHealthSpell.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class DrainHealthSpell : AbstractSpell
 {
 	[SerializeField] CharacterController _caster;
+	[SerializeField] float _maxDamage = 4.0f;
 	protected override void DamageTheCharacter(CharacterController character)
 	{
-		var drainedHP = character.HP;
+		var drainedHP = Mathf.Min(_maxDamage, character.HP);
 		character.DoDamage(drainedHP, this);
 		_caster.DoHeal(drainedHP);
 	}
